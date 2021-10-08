eb0b475849d7d772dbf7b3b9c2e399cf7a47eb12
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-10-08 18:43:56 +0000

    table schema

--- main.sql
+++ main.sql
@@ -1,3 +1,11 @@
 /*
 Julia just finished conducting a coding contest, and she needs your help assembling the leaderboard! Write a query to print the respective hacker_id and name of hackers who achieved full scores for more than one challenge. Order your output in descending order by the total number of challenges in which the hacker earned a full score. If more than one hacker received full scores in same number of challenges, then sort them by ascending hacker_id.
+
+Tables:
+Hackers: The hacker_id is the id of the hacker, and name is the name of the hacker. 
+
+Difficulty: The difficult_level is the level of difficulty of the challenge, and score is the score of the challenge for the difficulty level
+
+Challenges: The challenge_id is the id of the challenge, the hacker_id is the id of the hacker who created the challenge, and difficulty_level is the level of difficulty of the challenge.
 */
+
