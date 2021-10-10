2a7ff7a44f16b66d02c49a337b1ff85e319719a7
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-10-10 17:52:54 +0000

    solution

--- main.sql
+++ main.sql
@@ -9,10 +9,15 @@ Difficulty: The difficult_level is the level of difficulty of the challenge, and
 Challenges: The challenge_id is the id of the challenge, the hacker_id is the id of the hacker who created the challenge, and difficulty_level is the level of difficulty of the challenge.
 
 Submissions: The submission_id is the id of the submission, hacker_id is the id of the hacker who made the submission, challenge_id is the id of the challenge that the submission belongs to, and score is the score of the submission.
+having submissions.score>=difficulty.score
+select hackers.hacker_id,name,submissions.score,difficulty.score from hackers 
 */
 
-select hackers.hacker_id,name,submissions.score from hackers 
+select hackers.hacker_id,name from hackers 
 inner join submissions on hackers.hacker_id=submissions.hacker_id 
 inner join challenges on submissions.challenge_id=challenges.challenge_id 
-inner join difficulty on challenges.challenge_id=submissions.challenge_id 
-having hackers.hacker_id=40226
+inner join difficulty on challenges.challenge_id=submissions.challenge_id
+where submissions.score = difficulty.score and challenges.difficulty_level = difficulty.difficulty_level
+group by hackers.hacker_id,name
+having count(hackers.hacker_id)>1
+order by count(hackers.hacker_id) desc, hackers.hacker_id
